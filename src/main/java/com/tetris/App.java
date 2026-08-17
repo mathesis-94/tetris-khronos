b9cc0c8783c5c28bb08053ac2338b3d6619a8549
Morrigan <rains@tuta.com>
2026-08-17 16:01:39 +1000

    Comment out SplashScreen and Mainframe instantiation in App.java

--- src/main/java/com/tetris/App.java
+++ src/main/java/com/tetris/App.java
@@ -2,20 +2,20 @@ package com.tetris;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import com.tetris.view.SplashScreen;
-import com.tetris.view.Mainframe;
+// import com.tetris.view.SplashScreen;
+// import com.tetris.view.Mainframe;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Show splash screen
-        SplashScreen splash = new SplashScreen(4000);
-        splash.showSplash();
+ //       SplashScreen splash = new SplashScreen(4000);
+//        splash.showSplash();
 
         // Show main application window
-        Mainframe mainFrame = new Mainframe();
-        mainFrame.show(primaryStage);
+  //      Mainframe mainFrame = new Mainframe();
+    //    mainFrame.show(primaryStage);
     }
 
     public static void main(String[] args) {
